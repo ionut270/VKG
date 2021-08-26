1375639a3bb06d546de0fe77c2cd0d8fda45fee4
Iosif Negru <iosifneg@basware.com>
2021-08-26 22:25:29 +0300

    Mesaj de commit

--- interface/src/Components/header/header.jsx
+++ interface/src/Components/header/header.jsx
@@ -6,7 +6,8 @@ import { UserOutlined, ShoppingCartOutlined, HomeOutlined, ShopOutlined, InfoCir
 import Cartdropdown from '../cart/cart'
 
 import logo from '../../props/logo.png'
-import './header.css'
+import './portrait.css'
+import './landscape.css'
 
 const profileDropdown = (
     <Menu>
@@ -35,19 +36,23 @@ export default class header extends React.Component {
                 <div className="header_space"></div>
 
                 <Button className="header_option" href="/home">
-                    <HomeOutlined />Acasa
+                    <HomeOutlined /> 
+                    <div className="header_button_text">Acasa </div>
                 </Button>
                 <Button className="header_option" href="/shop">
-                    <ShopOutlined />Magazin
+                    <ShopOutlined />
+                    <div className="header_button_text">Magazin </div>
                 </Button>
 
                 <img className="header_logo" src={logo} alt="vikings logo" />
 
                 <Button className="header_option" href="/about">
-                    <InfoCircleOutlined />Despre
+                    <InfoCircleOutlined />
+                    <div className="header_button_text">Despre </div>
                 </Button>
                 <Button className="header_option" href="/contact">
-                    <MessageOutlined />Contact
+                    <MessageOutlined />
+                    <div className="header_button_text">Contact </div>
                 </Button>
 
                 <div className="header_extra">
